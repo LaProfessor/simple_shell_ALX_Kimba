71d0a524ca4e222e60ad15cb27dbfd50b159a7c6
Idowu Isaac LaProfessor <37228338+LaProfessor@users.noreply.github.com>
2023-07-17 12:48:50 +0100

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * hsh - main shell loop
+ * hsh - main shell loops
  * @info: the parameter & return info struct
- * @av: the argument vector from main()
+ * @av: the arg vector from main()
  *
- * Return: 0 on success, 1 on error, or error code
+ * Return: 0 on success, 1 on error, or error codes
  */
 int hsh(info_t *info, char **av)
 {
@@ -44,13 +44,13 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
+ * find_builtin - finds a builtin cmd
+ * @info: the parameter & return info structs
  *
  * Return: -1 if builtin not found,
  *			0 if builtin executed successfully,
  *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ *			-2 if builtin signals exits()
  */
 int find_builtin(info_t *info)
 {
@@ -79,9 +79,9 @@ int find_builtin(info_t *info)
 
 /**
  * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
+ * @info: the parameter & return info structs
  *
- * Return: void
+ * Return: voids
  */
 void find_cmd(info_t *info)
 {
@@ -121,7 +121,7 @@ void find_cmd(info_t *info)
 
 /**
  * fork_cmd - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
+ * @info: the parameter & return info structs
  *
  * Return: void
  */
