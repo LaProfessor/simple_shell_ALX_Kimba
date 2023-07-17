e5d6a500eaf451a148f3daae589c86a928de9e06
Idowu Isaac LaProfessor <37228338+LaProfessor@users.noreply.github.com>
2023-07-17 12:53:55 +0100

    Update vars.c

--- vars.c
+++ vars.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * is_chain - test if current char in buffer is a chain delimeter
- * @info: the parameter struct
- * @buf: the char buffer
+ * is_chain - test if current char in buffers is a chain delimeter
+ * @info: the parameter structs
+ * @buf: the char buffers
  * @p: address of current position in buf
  *
- * Return: 1 if chain delimeter, 0 otherwise
+ * Return: 1 if chain delimeter, 0 otherwises
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
@@ -24,7 +24,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		j++;
 		info->cmd_buf_type = CMD_AND;
 	}
-	else if (buf[j] == ';') /* found end of this command */
+	else if (buf[j] == ';') /* found end of this cmd */
 	{
 		buf[j] = 0; /* replace semicolon with null */
 		info->cmd_buf_type = CMD_CHAIN;
@@ -37,11 +37,11 @@ int is_chain(info_t *info, char *buf, size_t *p)
 
 /**
  * check_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
- * @buf: the char buffer
+ * @info: the parameter structs
+ * @buf: the char buffers
  * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
+ * @i: starting position in bufs
+ * @len: length of bufs
  *
  * Return: Void
  */
@@ -70,10 +70,10 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 }
 
 /**
- * replace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
+ * replace_alias - replaces an aliases in the tokenized str
+ * @info: the parameter structs
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 if replaced, 0 otherwises
  */
 int replace_alias(info_t *info)
 {
@@ -99,10 +99,10 @@ int replace_alias(info_t *info)
 }
 
 /**
- * replace_vars - replaces vars in the tokenized string
- * @info: the parameter struct
+ * replace_vars - replaces vars in the tokenized str
+ * @info: the parameter structs
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 if replaced, 0 otherwises
  */
 int replace_vars(info_t *info)
 {
@@ -140,9 +140,9 @@ int replace_vars(info_t *info)
 }
 
 /**
- * replace_string - replaces string
- * @old: address of old string
- * @new: new string
+ * replace_string - replaces str
+ * @old: address of old strs
+ * @new: new str
  *
  * Return: 1 if replaced, 0 otherwise
  */
