ec4454c094084063b6346873c468f0d840a78eb4
Idowu Isaac LaProfessor <37228338+LaProfessor@users.noreply.github.com>
2023-07-17 12:13:29 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ **_strncpy - copies a str
+ *@dest: the destination str to be copied to
+ *@src: the source str
+ *@n: the amount of char to be copied
+ *Return: the concatenated str
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -31,11 +31,11 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
+ **_strncat - concatenates two str together
+ *@dest: the first str
+ *@src: the second str
  *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ *Return: the concatenated str together
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -58,10 +58,10 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ **_strchr - locates a char in a str
+ *@s: the str to be parsed
+ *@c: the char to look for
+ *Return: (s) a pointer to the memory area s of char
  */
 char *_strchr(char *s, char c)
 {
