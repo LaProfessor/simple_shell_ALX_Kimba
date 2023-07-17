ebcff99817efc62af964b51cb181358773386bf6
Idowu Isaac LaProfessor <37228338+LaProfessor@users.noreply.github.com>
2023-07-17 14:20:05 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -2,9 +2,9 @@
 
 /**
  * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0 of all the commmand that has been ran on the console.
+ *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype of the console created.
+ *        constant function prototype.
  *  Return: Always 0
  */
 int _myhistory(info_t *info)
@@ -14,11 +14,11 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to str
+ * unset_alias - sets alias to string
  * @info: parameter struct
- * @str: the str alias
+ * @str: the string alias
  *
- * Return: Always 0 on success, 1 on errors
+ * Return: Always 0 on success, 1 on error
  */
 int unset_alias(info_t *info, char *str)
 {
@@ -37,9 +37,9 @@ int unset_alias(info_t *info, char *str)
 }
 
 /**
- * set_alias - sets alias to str
- * @info: para struct
- * @str: the str alias
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
  *
  * Return: Always 0 on success, 1 on error
  */
@@ -58,10 +58,10 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias str
+ * print_alias - prints an alias string
  * @node: the alias node
  *
- * Return: Always 0 on success, 1 on errors
+ * Return: Always 0 on success, 1 on error
  */
 int print_alias(list_t *node)
 {
@@ -71,7 +71,7 @@ int print_alias(list_t *node)
 	{
 		p = _strchr(node->str, '=');
 		for (a = node->str; a <= p; a++)
-			_putchar(*a);
+		_putchar(*a);
 		_putchar('\'');
 		_puts(p + 1);
 		_puts("'\n");
@@ -81,9 +81,9 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtins (man alias)
+ * _myalias - mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype of the console to be created.
+ *          constant function prototype.
  *  Return: Always 0
  */
 int _myalias(info_t *info)
